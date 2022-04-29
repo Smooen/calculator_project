0a3e55ef0f64ab8a3b307331b91e95c27720513d
Smooen <smooenus@gmail.com>
2022-04-29 12:23:53 +0200

    Add some logic, some restructuring

--- README.md
+++ README.md
@@ -4,5 +4,30 @@ This is going to be a functioning calculator, very cool.
 
 To-do steps:
 
-Add buttonpress functionality for number keys
-store key value in variable(s)
+Implement Calculation after pressing equals button or second operator
+Make it so adding the numbers will not concatenate them instead
+
+Have it be impossible to input operant twice in a row
+
+
+
+Flow of calc:
+
+user inputs first number
+
+user inputs one operant 
+
+user inputs second number
+
+if user inputs another op, calculate result
+show result
+
+...
+user inputs another operant
+
+user inputs another number
+...
+
+Press Equals to calculate result
+
+Clear calulator -> reset all values and clear screen
--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let operant = '';
+
 function add(num1, num2){
     return num1 + num2;
 }
@@ -14,7 +18,7 @@ function divide(num1, num2){
     return num1 / num2;
 }
 
-function operate(op, num1, num2){
+function operate(num1, op, num2){
     //input needs to be char/string to work in browser console
     switch(op){
         case '+': return add(num1, num2);
@@ -32,11 +36,42 @@ function operate(op, num1, num2){
 
 // button1.addEventListener('click', () => console.log("ONE"));
 
-let buttons = document.querySelectorAll('button');
+let numberButtons = document.querySelectorAll('.keys');
+let opButtons = document.querySelectorAll('.op');
+let equalsButton = document.getElementById('equals');
+let clearButton = document.getElementById('clear');
 
-buttons.forEach(function(btn){
+
+equalsButton.addEventListener('click', () => {
+    let result = operate(firstNumber, operant, secondNumber);
+    console.log(result);
+});
+
+clearButton.addEventListener('click', () => {
+    firstNumber = 0;
+    secondNumber = 0;
+    operant = '';
+    document.querySelector(".message").innerHTML = '';
+});
+
+numberButtons.forEach(function(btn){
     btn.addEventListener('click', function(){
-        //sets message html content to html content of btn
-        document.querySelector(".message").innerHTML = btn.innerHTML;
+        if(operant === ''){
+            firstNumber += btn.innerHTML;
+        }
+        else{
+            secondNumber += btn.innerHTML;
+        }
+        document.querySelector(".message").innerHTML += btn.innerHTML;
+        console.log("first number: " + firstNumber);
+        console.log("second num: " + secondNumber);
+    });
+});
+
+opButtons.forEach(function(btn){
+    btn.addEventListener('click', function(){
+        operant = btn.innerHTML;
+        document.querySelector(".message").innerHTML += btn.innerHTML;
+        console.log(operant);
     });
 });
